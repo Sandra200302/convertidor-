1dacad807b8bac0faa276bda519c35a50551bb13
Sandra200302 <62914845+Sandra200302@users.noreply.github.com>
2022-01-29 21:21:40 -0600

    Add files via upload

---
+++ convertidordeunidades.py
@@ -0,0 +1,23 @@
+print("Conversion de unidades")
+print("Pulgadas a centimetros ")
+pulgadas= input("Da las pulgadas")
+cm=float(pulgadas)*2.54
+print("cm= ",cm)
+
+print("centimetros a pulgadas ")
+cmp= input("Da los centimetros")
+pulc=float(cmp)/2.54
+print("pulgadas= ", pulc)
+
+print("m/s a K/h")
+ms= input("Da los metros sobre segundo ")
+km=int(ms)*3600/1000
+print("K/h = ", km)
+
+print("K/h a m/s")
+kmd= input("Da los kilometros sobre hora")
+msd=int(kmd)*1000/3600
+print("m/s = ", msd)
+
+
+
